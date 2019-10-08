be8d523490170d014527503f664f0d380faad5b8
defaultuser0 <defaultuser0@DESKTOP-FDVUPSA>
2019-10-08 13:23:34 +0900

    init

---
+++ src/kr/co/itcen/fileupload/exception/FileuploadException.java
@@ -0,0 +1,7 @@
+package kr.co.itcen.fileupload.exception;
+
+public class FileuploadException extends RuntimeException {
+	public void FileUploadException(String message) {
+		super.getMessage();
+	}
+}
